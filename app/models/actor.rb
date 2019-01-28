ef4140708a2234c8476c8dd95d8c20fc1608d98f
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-28 11:14:48 -0500

    Crea la migración

---
+++ app/models/actor.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer          not null, primary key
+#  name        :string(100)
+#  bio         :text
+#  birth_date  :date
+#  birth_place :string(100)
+#  image_url   :string
+#  death_date  :date
+#  death_place :string(100)
+#  alive       :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Actor < ApplicationRecord
+end
